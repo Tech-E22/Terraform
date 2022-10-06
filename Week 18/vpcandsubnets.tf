43ddc263e851a54d7279dd3cf4c2e86d4fd2db99
Erika Anderson <emanderson090713@gmail.com>
2022-10-06 23:01:12 +0000

    edits

---
+++ Week 18/vpcandsubnets.tf
@@ -0,0 +1,46 @@
+# Create a VPC
+resource "aws_vpc" "week18" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "week18"
+  }
+}
+
+# Create 2 Public Subnets
+resource "aws_subnet" "public_subnet1a_week18" {
+  vpc_id            = aws_vpc.week18.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "public subnet 1a_week18"
+  }
+}
+
+resource "aws_subnet" "public_subnet1b_week18" {
+  vpc_id            = aws_vpc.week18.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "public subnet 1b_week18"
+  }
+}
+
+# Create 2 Private Subnets
+resource "aws_subnet" "private_subnet_1a_week18" {
+  vpc_id                  = aws_vpc.week18.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "private subnet 1a_week18"
+  }
+}
+resource "aws_subnet" "private_subnet_1b_week18" {
+  vpc_id                  = aws_vpc.week18.id
+  cidr_block              = "10.0.4.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "private subnet 1b_week18"
+  }
+}
